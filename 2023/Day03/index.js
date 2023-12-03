a8170d8ae2a89870c86ccd3840e399ee0040921a
Stacey Fenton <stacey.fenton@vodafone.com>
2023-12-03 13:51:06 +0000

    Day 3 part 1 solution

---
+++ 2023/Day03/index.js
@@ -0,0 +1,93 @@
+import input from './input.js';
+
+const lines = input.split('\n');
+const numbersWithAdjacentSymbols = [];
+const regexNumbers = /\d+/g;
+const regexSymbols = /[^.\d]/g;
+
+// find all positions of a symbol in a string
+const findAllSymbolPositions = (str, symbol) => {
+  const positions = [];
+  let index = str.indexOf(symbol);
+  while (index !== -1) {
+    positions.push(index);
+    index = str.indexOf(symbol, index + 1);
+  }
+  return positions;
+}
+
+// find adjacent symbols in the same row, row above, and row below
+const findAdjacentSymbols = (inputArray, currentRow, currentPosition, numberLength) => {
+  const adjacentSymbols = [];
+
+  // Check the same row for adjacent symbols
+  const currentRowSymbols = inputArray[currentRow].match(regexSymbols);
+  if (currentRowSymbols) {
+    for (const symbol of currentRowSymbols) {
+      const symbolPositions = findAllSymbolPositions(inputArray[currentRow], symbol);
+      for (const symbolPosition of symbolPositions) {
+        if (symbolPosition >= currentPosition - 1 && symbolPosition < (currentPosition + numberLength + 1)) {
+          adjacentSymbols.push({ value: symbol, position: symbolPosition, row: currentRow });
+        }
+      }
+    }
+  }
+
+  // Check the row above for adjacent symbols
+  if (currentRow > 0) {
+    const aboveRowSymbols = inputArray[currentRow - 1].match(regexSymbols);
+    if (aboveRowSymbols) {
+      for (const symbol of aboveRowSymbols) {
+        const symbolPositions = findAllSymbolPositions(inputArray[currentRow - 1], symbol);
+        for (const symbolPosition of symbolPositions) {
+          if (symbolPosition >= currentPosition - 1 && symbolPosition < (currentPosition + numberLength + 1)) {
+            adjacentSymbols.push({ value: symbol, position: symbolPosition, row: currentRow - 1 });
+          }
+        }
+      }
+    }
+  }
+
+  // Check the row below for adjacent symbols
+  if (currentRow < inputArray.length - 1) {
+    const belowRowSymbols = inputArray[currentRow + 1].match(regexSymbols);
+    if (belowRowSymbols) {
+      for (const symbol of belowRowSymbols) {
+        const symbolPositions = findAllSymbolPositions(inputArray[currentRow + 1], symbol);
+        for (const symbolPosition of symbolPositions) {
+          if (symbolPosition >= currentPosition - 1 && symbolPosition < (currentPosition + numberLength + 1)) {
+            adjacentSymbols.push({ value: symbol, position: symbolPosition, row: currentRow + 1 });
+          }
+        }
+      }
+    }
+  }
+
+  return adjacentSymbols;
+}
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+
+  // Extract numbers
+  let matchNumbers;
+  while ((matchNumbers = regexNumbers.exec(line)) !== null) {
+    const numberInfo = {
+      value: parseInt(matchNumbers[0]),
+      position: matchNumbers.index,
+      row: i
+    };
+   
+    // Check for adjacent symbols in the same row, row above, and row below
+    const adjacentSymbols = findAdjacentSymbols(lines, i, matchNumbers.index, numberInfo.value.toString().length);
+    if (adjacentSymbols.length > 0) {
+      numberInfo.adjacentSymbols = adjacentSymbols;
+      numbersWithAdjacentSymbols.push(numberInfo);
+    }
+  }
+}
+
+// Add up the part numbers 
+const sumOfPartNumbers = numbersWithAdjacentSymbols.reduce((sum, obj) => sum + obj.value, 0);
+
+console.log(sumOfPartNumbers);
