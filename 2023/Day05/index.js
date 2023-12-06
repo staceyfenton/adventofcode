63f8805c316181fbb44c6f3461aad049d9c279c5
Stacey Fenton <stacey.fenton@vodafone.com>
2023-12-06 17:24:35 +0000

    Day 5 part 2

--- 2023/Day05/index.js
+++ 2023/Day05/index.js
@@ -12,10 +12,9 @@ const findLocationFromSeed = (seed) => {
   let current = seed;
   for (const map of maps) {
     for (const [destinationStart, sourceStart, rangeLength] of map) {
-      const maxSeed = sourceStart + rangeLength;
+      const maxSeed = sourceStart + (rangeLength - 1);
       const minSeed = sourceStart;
 
-      // console.log(`current: ${current}, min: ${minSeed}, max: ${maxSeed}`);
       if (current >= minSeed && current <= maxSeed) {
         current = destinationStart + (current - minSeed);
         break;
@@ -31,3 +30,50 @@ for (const seed of seeds) {
 
 const min = Math.min(...locations);
 console.log(min);
+
+/* ----- Part 2 ----- */
+
+const seedRanges = [];
+
+for (let i = 0; i < seeds.length; i += 2) {
+  const minSeed = seeds[i];
+  const maxSeed = minSeed + seeds[i + 1] - 1;
+
+  seedRanges.push({ min: minSeed, max: maxSeed });
+}
+
+const findSeedFromLocation = (location) => {
+  let seed = location;
+  for (let i = (maps.length - 1); i > -1; i--) {
+    for (const [destinationStart, sourceStart, rangeLength] of maps[i]) {
+      const maxSource = destinationStart + (rangeLength - 1);
+      const minSource = destinationStart;
+
+      if (seed <= maxSource && seed >= minSource) {
+        seed = sourceStart + (seed - minSource);
+        break;
+      }
+    }
+  }
+  return seed;
+}
+
+const isSeedInRange = (seed) => {
+  for (const range of seedRanges) {
+    if (seed >= range.min && seed <= range.max) {
+      return true;
+    }
+  }
+  return false; 
+}
+
+let currentLocation = 0;
+
+while (true) {
+  currentLocation++;
+  const seed = findSeedFromLocation(currentLocation);
+  if (isSeedInRange(seed)) {
+    console.log(currentLocation);
+    break;
+  }
+}
