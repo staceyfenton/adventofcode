3a6adc9f3898f566a8cc57bf31b845564aa77065
Stacey Fenton <stacey.fenton@vodafone.com>
2023-12-09 21:05:41 +0000

    Day 8 part 2

--- 2023/Day08/index.js
+++ 2023/Day08/index.js
@@ -1,16 +1,21 @@
 import input from './input.js';
+// const input = `LR
+
+// 11A = (11B, XXX)
+// 11B = (XXX, 11Z)
+// 11Z = (11B, XXX)
+// 22A = (22B, XXX)
+// 22B = (22C, 22C)
+// 22C = (22Z, 22Z)
+// 22Z = (22B, 22B)
+// XXX = (XXX, XXX)`;
 
 const lines = input.split('\n');
 const directions = lines[0].split('');
 const network = lines.slice(2);
 const parseNetwork = network.map(n => n.split(' = '));
-// console.log(parseNetwork);
-
-// console.log(directions);
-// console.log(network);
 
 const getNextNode = (nodes, direction) => {
-
   const [leftNode, rightNode] = nodes[1].slice(1, -1).split(', ');
   return direction === 'L' ? leftNode : rightNode;
 }
@@ -22,7 +27,7 @@ const getNumberOfSteps = (location = 'AAA') => {
     const direction = directions[steps++ % directions.length];
     const node = parseNetwork.find(item => item[0] === location);
     location = getNextNode(node, direction);
-    if (location === 'ZZZ') {
+    if (location.split('')[2] === 'Z') {
       break;
     }
   }
@@ -30,3 +35,17 @@ const getNumberOfSteps = (location = 'AAA') => {
 }
 
 console.log(getNumberOfSteps());
+
+/* Part 2 */
+
+// find possible starting locations ending in A
+const startingLocations = parseNetwork.filter(item => item[0].endsWith('A')).map(item => item[0]);
+
+// calculate the Greatest Common Divisor (GCD)
+const calculateGCD = (num1, num2) => (num2 === 0 ? num1 : calculateGCD(num2, num1 % num2));
+
+// calculate the Least Common Multiple (LCM)
+const calculateLCM = (num1, num2) => (num1 / calculateGCD(num1, num2)) * num2;
+
+const steps = startingLocations.map(getNumberOfSteps).reduce(calculateLCM);
+console.log(steps);
