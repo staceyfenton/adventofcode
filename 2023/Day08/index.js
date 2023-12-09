f292fd395a8561ebf1e62cf8aaa1e6714b6dfc1f
Stacey Fenton <stacey.fenton@vodafone.com>
2023-12-09 21:06:23 +0000

    Day 8 cleanup

--- 2023/Day08/index.js
+++ 2023/Day08/index.js
@@ -1,14 +1,4 @@
 import input from './input.js';
-// const input = `LR
-
-// 11A = (11B, XXX)
-// 11B = (XXX, 11Z)
-// 11Z = (11B, XXX)
-// 22A = (22B, XXX)
-// 22B = (22C, 22C)
-// 22C = (22Z, 22Z)
-// 22Z = (22B, 22B)
-// XXX = (XXX, XXX)`;
 
 const lines = input.split('\n');
 const directions = lines[0].split('');
