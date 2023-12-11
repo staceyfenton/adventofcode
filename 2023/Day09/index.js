cc0165533d4619456bc941b8541430e9f6060273
Stacey Fenton <stacey.fenton@vodafone.com>
2023-12-11 21:13:44 +0000

    Day 9 part 2

--- 2023/Day09/index.js
+++ 2023/Day09/index.js
@@ -4,7 +4,7 @@ import input from './input.js';
 const histories = input.split('\n').map(history => history.split(' ').map(str => parseInt(str)));
 
 // Function to find the next number in a sequence recursively
-const findNextInSequence = (history) => {
+const findNextInSequence = (history, isPartTwo = false) => {
   let differences = [];
   let nextNumber = 0;
 
@@ -16,15 +16,17 @@ const findNextInSequence = (history) => {
 
   // If the next number is not zero, continue the sequence recursively
   if(nextNumber !== 0) {
-    nextNumber = findNextInSequence(differences);
+    nextNumber = findNextInSequence(differences, isPartTwo);
   }
 
-  // Return the sum of the next number and the last number in the original sequence
-  return nextNumber + history[history.length-1];
+  // If part one, return the sum of the next number and the last number in the original sequence
+  // If part two, return the first number in the original sequence minus the next number
+  return isPartTwo ? history[0] - nextNumber : nextNumber + history[history.length-1];
 };
 
 // Calculate the sum of the next numbers in each history
-const sum = histories.reduce((acc, history) => acc + findNextInSequence(history), 0);
+const partOneSum = histories.reduce((acc, history) => acc + findNextInSequence(history), 0);
+const partTwoSum = histories.reduce((acc, history) => acc + findNextInSequence(history, true), 0);
 
-// Output the final sum
-console.log(sum);
+// Output the final sums
+console.log(partOneSum, partTwoSum);
